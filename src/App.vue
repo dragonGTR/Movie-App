1d325c1bc52ee7dfcb595fe9a5a6567ef095b7b8
dragonGTR <77241091+dragonGTR@users.noreply.github.com>
2021-06-07 11:41:26 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <header>
+    <div id="header">
+      <h1 style="color:white"><span id="col">Vue</span> Movies</h1>
+    </div>
+  </header>
+  <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0px;
+  padding: 0px;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  background-color: #73a5c6;
+}
+#header {
+  display: flex;
+  justify-content: center;
+  background-color: #23395d;
+  padding: 10px;
+}
+#col {
+  color: #00ff00;
 }
 </style>
